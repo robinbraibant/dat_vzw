50b3d6dd10751775af6a5060b79360f0ccd52111
Robin Braibant <robin.braibant@aplinedigital.be>
2023-12-09 12:15:53 +0100

    minor changes

--- config/matrixmate.php
+++ config/matrixmate.php
@@ -1,44 +1,4 @@
 <?php
 
 return [
-    "fields" => [
-        "contentBuilder" => [
-            "groups" => [
-                [
-                    "label" => "Content",
-                    "types" => ["redactor", "textImage", "columns", "titleBlock", "coreImage", "image", "gallery", "video", "form", "usps"]
-                ],
-                [
-                    "label" => "Contact",
-                    "types" => ["contact"]
-                ],
-                [
-                    "label" => "Blog",
-                    "types" => ["blogAll", "blogLatest"]
-                ],
-                [
-                    "label" => "Cases",
-                    "types" => ["casesAll", "casesLatest"]
-                ],
-                [
-                    "label" => "Diensten",
-                    "types" => ["servicesAll", "servicesLatest"]
-                ],
-                [
-                    "label" => "Entries overzicht",
-                    "types" => [
-                        "overviews"
-                    ]
-                ],
-                [
-                    "label" => "Klanten",
-                    "types" => ["clientsAll", "reviews", "reviewImage"]
-                ],
-                [
-                    "label" => "Team",
-                    "types" => ["team"]
-                ]
-            ]
-        ]
-    ]
 ];
